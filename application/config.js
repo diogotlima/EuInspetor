0cadce508566ee72e4fab30c62ef808cfbf8c96c
diogolima22 <diogo@MacBook-Pro-de-Diogo.local>
2015-12-02 19:57:22 -0300

    conect firebase
    Commit

--- application/config.js
+++ application/config.js
@@ -1,23 +1,17 @@
 /**
  * Created by diogo on 01/12/15.
  */
-
 (function () {
-    'use strict';
-
-    angular.module('mwa').constant('SETTINGS', {
-        'VERSION': '0.0.1',
-        'CURR_ENV': 'dev',
-        'AUTH_USER': 'mwa-user',
-        'SERVICE_URL': 'https://euinspetor.firebaseio.com'
+    angular.module('mwa').constant('APP_SETTINGS', {
+        "FIREBASE_URL": "https://euinspetor.firebaseio.com"
     });
 
-    angular.module('mwa').run(function ($rootScope, $location, SETTINGS) {
-
+    angular.module('insta').run(function ($rootScope, $location) {
+        $rootScope.user = null;
 
         $rootScope.$on("$routeChangeStart", function (event, next, current) {
             if ($rootScope.user == null) {
-                $location.path('/login');
+                $location.path("/login");
             }
         });
     });
--- application/factorys/user-factory.js
+++ application/factorys/user-factory.js
@@ -5,9 +5,9 @@
     'use strict';
     angular.module('mwa').factory('LoginFactory', LoginFactory);
 
-    LoginFactory.$inject = ['$firebaseAuth', 'SETTINGS'];
+    LoginFactory.$inject = ['$http','$firebaseAuth', 'SETTINGS'];
 
-    function LoginFactory($firebaseAuth, SETTINGS) {
+    function LoginFactory($http,$firebaseAuth, SETTINGS) {
         return {
             login: login
         };
@@ -15,15 +15,49 @@
 
         function login(data) {
 
-            var url = SETTINGS.SERVICE_URL ;
+            var url = new $http.Firebase(SETTINGS.SERVICE_URL);
             var fbAuth = $firebaseAuth(url);
             return fbAuth.$authWithPassword({ email:data.email ,password: data.password});
 
-
         }
 
+    }
+})();
 
+appGeral.controller("LoginController", function($scope, $firebaseAuth, $location) {
 
+    $scope.login = function(username, password) {
+        var fbAuth = $firebaseAuth(fb);
+        fbAuth.$authWithPassword({
+            email: username,
+            password: password
+        }).then(function(authData) {
+            $scope.modalLogin.hide();
+            $location.path("/app/denuncias");
+
+        }).catch(function(error) {
+            console.error("ERROR: " + error);
+            alert("Tente novamente " + error);
+        });
     }
 
-})();
+
+    $scope.register = function(username, password) {
+        var fbAuth = $firebaseAuth(fb);
+        fbAuth.$createUser({email: username, password: password}).then(function() {
+            return fbAuth.$authWithPassword({
+                email: username,
+                password: password
+            });
+        }).then(function(authData) {
+            $scope.modalCadastro.hide();
+            $scope.modalLogin.hide();
+            alert("Seu cadastro foi efetuado com sucesso!");
+            $location.path("/app/denuncias");
+        }).catch(function(error) {
+            console.error("ERROR " + error);
+            alert("Ocorreu o seguinte " + error);
+        });
+    }
+
+});
