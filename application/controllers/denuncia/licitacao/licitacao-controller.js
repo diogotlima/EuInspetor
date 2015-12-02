8c8c56241ccb30e19cf057063fbcce98936ab228
André Fillype <anfyps@hotmail.com>
2015-12-02 14:18:10 -0300

    Controllers de obras, licitações e transportes

--- application/controllers/denuncia/licitacao/licitacao-controller.js
+++ application/controllers/denuncia/licitacao/licitacao-controller.js
@@ -1,3 +1,14 @@
 /**
  * Created by andrefillype on 02/12/15.
  */
+
+(function () {
+    'use strict';
+    angular.module('mwa').controller('LicitacaoController', LicitacaoController);
+
+    LicitacaoController.$inject = ['$location', '$rootScope', 'SETTINGS', 'LicitacaoFactory'];
+
+    function LicitacaoController($location, $rootScope, SETTINGS, LicitacaoFactory) {
+
+    };
+})();
