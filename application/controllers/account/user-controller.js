0671f0f48efbaa402067b305cebd902fcf678d8c
jorgedantas <jorgedantas2@gmail.com>
2015-12-03 11:13:52 -0200

    Merge + Home

--- application/controllers/account/user-controller.js
+++ application/controllers/account/user-controller.js
@@ -31,7 +31,7 @@
                     $rootScope.user = {
                         email: vm.login.email
                     };
-                    $location.path('/obras');
+                    $location.path('/inicial');
                     $scope.$apply();
                     alert(email);
                 }
